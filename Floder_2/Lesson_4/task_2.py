4dd2610b5daf3ce852566ae7037142c561065863
Minttoffy <153549531+Minttoffy@users.noreply.github.com>
2024-10-23 20:12:12 +0300

    Lesson_4
    Задания 1,2,3,4

---
+++ Floder_2/Lesson_4/task_2.py
@@ -0,0 +1,5 @@
+#task_2. Площадь магазина
+print("Задание 2")
+length = float(input("Длина в метрах = "))
+width = float(input("Ширина в метрах  = "))
+print("Площадь = ", round(length * width), "м^2")
